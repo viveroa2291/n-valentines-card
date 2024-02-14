7578a8592b027b55a8aa26b6ba255781848214b6
viveroa2291 <viveroa2291@uwec.edu>
2024-02-14 14:51:57 -0600

    Final Product

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import me from './images/me.jpeg';
 function App() {
+  const [envelopeState, setEnvelopeState] = useState({
+    isInnerOpen: false,
+    isOuterOpen: false,
+    isHeartBtnHidden: false,
+    isCloseBtnVisible: false,
+    isMessage1Hidden: false,
+    isMessage2Visible: false,
+    isImageVisible: false,
+  });
+  const openEnvelope = () => {
+      setEnvelopeState((prevState) => ({
+        ...prevState, 
+        isInnerOpen: true,
+        isOuterOpen: true,
+        isHeartBtnHidden: true,
+        isCloseBtnVisible: true,
+        isMessage1Hidden: true,
+        isMessage2Visible: true,
+        isImageVisible: true,
+        selectedImage: me
+      }))
+    }
+  const closeEnvelope = () => {
+    setEnvelopeState((prevState) => ({ 
+      ...prevState, 
+      isInnerOpen: false,
+      isOuterOpen: false,
+      isHeartBtnHidden: false,
+      isCloseBtnVisible: false,
+      isMessage1Hidden: false,
+      isMessage2Visible: false,
+      isImageVisible: false
+  }));
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article>
+        <div className="envelope-container">
+          <p className={`message message-1 ${envelopeState.isMessage1Hidden ? 'hide' : ''}`}>
+            <strong>Happy Valentine's Day!</strong>
+            <br/><br/>Click on the heart seal to open the envelope.
+          </p>
+          <span className={`note ${envelopeState.isMessage2Visible ? 'show' : 'hide'} `}>
+            <span className='picture'>
+              <img className={`imageChosen ${envelopeState.isImageVisible ? 'show' : ''}`} src={envelopeState.selectedImage} alt=''/>
+            </span>
+            <p className={`message message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>Hello Nikki,</p>
+            <p className={`valentines-message message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>Cheers to all the crazy fun we've had. Your friendship is like a rare gem. It shines bright and brings joy to my life. Looking forward to more! Happy Valentine's Day! 💌</p>
+            <p className={`sincerely message-2 ${envelopeState.isMessage2Visible ? 'show' : ''}`}>Love, Adan</p>
+          </span>
+          <button className={`heart-btn otherButton ${envelopeState.isHeartBtnHidden ? 'hide' : 'show'}`} onClick={openEnvelope}>&#10084;</button>
+          <button className={`close-btn otherButton ${envelopeState.isCloseBtnVisible ? 'show' : ''}`} onClick={closeEnvelope}>close envelope</button>
+          <div className="envelope-flap">
+            <svg className="inner">
+              <polygon className={`inner-polygon ${envelopeState.isInnerOpen ? 'inner-open' : ''}`} points="5,0 345,0 175,145"></polygon>
+            </svg>
+            <svg className="outer" id="outer">
+              <polygon className={`outer-polygon ${envelopeState.isOuterOpen ? 'outer-open' : ''}`} points="5,150 345,150 175,0"></polygon>
+            </svg>
+          </div>
+          <div className="envelope-base"></div>
+        </div>
+      </article>
     </div>
   );
 }
